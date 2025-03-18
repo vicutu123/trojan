356b8ebec3ca6dba4ba9fcccac1367df51b551b3
vicutu123 <47731688+vicutu123@users.noreply.github.com>
2025-03-18 13:05:16 +0800

    Update xrayr_install.sh

--- xrayr_install.sh
+++ xrayr_install.sh
@@ -102,17 +102,16 @@ Nodes:
       ListenIP: 0.0.0.0 # IP address you want to listen
       SendIP: 0.0.0.0 # IP address you want to send pacakage
       UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: true # Use custom DNS config, Please ensure that you set the dns.json well
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
       DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      EnableProxyProtocol: true # Only works for WebSocket and TCP
-      EnableFallback: true # Only support for Trojan and Vless
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
       FallBackConfigs:  # Support multiple fallbacks
-        - 
+        -
           SNI: # TLS SNI(Server Name Indication), Empty for any
           Path: # HTTP PATH, Empty for any
           Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
           ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
-	  
       CertConfig:
         CertMode: file # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
         CertDomain: "111.111" # Domain to cert
